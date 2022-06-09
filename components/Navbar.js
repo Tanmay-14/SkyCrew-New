7ea1d535d8714aef0c338b9fdd1e00a804309a6f
Tanmay-14 <tbag1401@gmail.com>
2022-06-09 19:40:47 +0530

    main

---
+++ components/Navbar.js
@@ -0,0 +1,38 @@
+import styles from '../styles/Navbar.module.css'
+import Link from 'next/link'
+
+const Navbar = () => {
+  return (
+    <div className={styles.nav}>
+        <div className={styles.logo}>
+            <Link href="/">
+                SkyCrew
+            </Link>
+        </div>
+        <div className={styles.links}>
+            <ul>
+                <li>
+                    <Link href="/categories">
+                        <span>Categories</span> 
+                    </Link>
+                </li>
+                <li>
+                    <Link href="/challenges">
+                        <span>Challenges</span> 
+                    </Link>
+                </li>
+                <li>
+                    <Link href="/jobs">
+                        <span>Jobs</span> 
+                    </Link>
+                </li>
+            </ul>
+            <Link href="/profile">
+                <input type="submit" value="Connect" className={styles.btn}/>
+            </Link>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
